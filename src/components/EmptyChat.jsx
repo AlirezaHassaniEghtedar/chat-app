f629095dfb5b8a44f134f180d35039ddbeeb6d0e
Alireza Hassani Eghtedar <alirezaprogrammer2001@gmail.com>
2026-08-15 12:52:56 +0330

    [FIX] typo

--- src/components/EmptyChat.jsx
+++ src/components/EmptyChat.jsx
@@ -5,7 +5,7 @@ export default function EmptyChat() {
     <div className="w-full h-full grid place-content-center justify-items-center gap-4 text-center">
       <MessagesSquare className="size-16 animate-bounce opacity-70" />
       <p className="capitalize text-2xl font-medium opacity-70">
-        no messages here yet type
+        no messages here yet
       </p>
       <p className="text-lg font-medium opacity-90 animate-pulse">
         send a message to start chatting...
